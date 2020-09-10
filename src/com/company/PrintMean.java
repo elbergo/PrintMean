fe256083ce56090f78fd2a9a7981abd1bd35489f
Andreas Berg <andreas.berg2@student.kyh.se>
2020-09-10 13:37:21 +0200

    Revert "added comment"
    This reverts commit d6da185015e3e7099b09c466fe3cd13981b7e079.

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -15,7 +15,7 @@ public class PrintMean {
 
     public static String meanCalc(double a, double b, double c, double d) { // ändrar från int till double
         // göra en sträng av mean värdet
-        //lösa problemet med heltalsdivision hej hej
+        //lösa problemet med heltalsdivision
         double mean = (a + b + c + d ) / 4;
         String meanString = String.valueOf(mean);
 
