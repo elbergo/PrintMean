5aef8ae3787d71c952a583e281dfaeb7868ee9c5
Andreas Berg <andreas.berg2@student.kyh.se>
2020-09-10 14:52:55 +0200

    added  Mean of 1, 1, 3, 5:

--- src/com/company/PrintMean.java
+++ src/com/company/PrintMean.java
@@ -5,7 +5,7 @@ public class PrintMean {
     public static void main(String[] args) {
         //heltalsdivision (skriver ut int istället för komma-doubles)
         //
-
+        System.out.println("Mean of 1, 1, 3, 5: " + meanCalc(1,1,3,5));
         System.out.println("Mean of 1, 1, 3, 3: " + meanCalc(1,1,3,3));
         System.out.println("Mean of 1, 2, 3, 4: " + meanCalc(1,2,3, 4));
         System.out.println("Mean of 1, -2, 3, -4: " + meanCalc(1,2,3,4));
